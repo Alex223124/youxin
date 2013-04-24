9fd1bbef8a7826d85b7669576a062f2b613d7d79
fahchen <dev.fah@gmail.com>
2013-04-24 10:55:36 +0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Youxin::Application.config.secret_token = 'aa51d9a0b2ab05c21e537ee359dbfc2b346d6f1f5eeef74db7795e48490f0dc142bf7b6bbda8c2e7413b8f6cdf9f00b3119a820edd4108d1dc943766c7062f5c'
