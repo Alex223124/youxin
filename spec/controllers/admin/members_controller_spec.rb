e3ac562e9169f39858ac3014a7087b65a6437c59
fahchen <06fahchen@gmail.com>
2013-05-07 15:30:33 +0800

    add member to organization in controller

--- spec/controllers/admin/members_controller_spec.rb
+++ spec/controllers/admin/members_controller_spec.rb
@@ -1,26 +1,41 @@
 require 'spec_helper'
 
 describe Admin::MembersController do
-
+  before(:each) do
+    @organization = create :organization
+    @user = create :user
+    @another_user = create :user
+    request.env["HTTP_REFERER"] = admin_organizations_url
+  end
   describe "GET 'index'" do
     it "returns http success" do
-      get 'index', id: 1
-      response.should be_success
-    end
-  end
-
-  describe "PUT 'update'" do
-    it "returns http success" do
-      put 'update', id: 1
+      get 'index', id: @organization.id
       response.should be_success
     end
   end
 
   describe "DELETE 'destroy'" do
     it "returns http success" do
-      delete 'destroy', id: 1
-      response.should be_success
+      @organization.push_members([@user.id, @another_user.id])
+      expect {
+        delete 'destroy', {
+          id: @organization.id,
+          member_ids: [@user.id, @another_user.id]
+        }
+      }.to change{ @organization.reload.members.count }.by(-2)
     end
   end
+
+  describe "PUT 'update'" do
+    it "should add members to organization" do
+      expect {
+        put :update, {
+          id: @organization.id,
+          member_ids: [@user.id, @another_user.id]
+        }
+      }.to change{ @organization.reload.members.count }.by(2)
+    end
+  end
+
 
 end
