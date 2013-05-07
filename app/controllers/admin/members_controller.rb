324056b8a4c5464dd7b407931ead94a7ebded933
fahchen <06fahchen@gmail.com>
2013-05-07 15:30:33 +0800

    add member to organization in controller

--- app/controllers/admin/members_controller.rb
+++ app/controllers/admin/members_controller.rb
@@ -1,12 +1,21 @@
 class Admin::MembersController < ApplicationController
+  before_filter :find_organization
   def index
   end
 
   def update
-    render nothing: true
+    @organization.push_members(params[:member_ids])
+    redirect_to members_admin_organization_path(@organization)
   end
 
   def destroy
-    render nothing: true
+    @organization.pull_members(params[:member_ids])
+    redirect_to members_admin_organization_path(@organization)
+  end
+
+  private
+  def find_organization
+    @organization = Organization.where(id: params[:id]).first
+    redirect_to :back, flash: { alert: 'Organization does not exist' } unless @organization
   end
 end
