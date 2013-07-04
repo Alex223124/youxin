006dcd350140342b065ca9f2108d0c7ad80fe6ba
fahchen <dev.fah@gmail.com>
2013-07-04 15:21:53 +0800

    support version for attachment downloads

--- app/controllers/attachments_controller.rb
+++ app/controllers/attachments_controller.rb
@@ -1,11 +1,27 @@
 class AttachmentsController < ApplicationController
   before_filter do
     @attachment = Attachment::Base.find(params[:id])
-    access_denied! unless @attachment && can?(current_user, :download, @attachment)
+    unless @attachment && can?(current_user, :download, @attachment)
+      access_denied!
+      return false
+    end
+    if @attachment.image? && params['version'].present?
+      begin
+        @path = @attachment.storage.send(params['version']).path
+      rescue => e
+        access_denied!
+        return false
+      end
+      @file_name = "#{params['version']}_#{@attachment.file_name}"
+    else
+      @path = @attachment.storage.path
+      @file_name = @attachment.file_name
+    end
+    @file_type = @attachment.file_type
   end
 
   def show
-    send_file @attachment.storage.path, filename: @attachment.file_name, type: @attachment.file_type, disposition: 'attachment'
+    send_file @path, filename: @file_name, type: @file_type, disposition: 'attachment'
   end
 end
 
--- spec/controllers/attachments_controller_spec.rb
+++ spec/controllers/attachments_controller_spec.rb
@@ -15,25 +15,48 @@ describe AttachmentsController do
 
     @file_path = Rails.root.join("spec/factories/data/attachment_file.txt")
     @file = Rack::Test::UploadedFile.new(@file_path, 'text/plain')
+    @image_path = Rails.root.join("spec/factories/data/attachment_image.png")
+    @image = Rack::Test::UploadedFile.new(@image_path, 'image/png')
 
     attrs = attributes_for(:post).merge({
       organization_ids: [@organization.id],
     })
     @post = @admin.posts.create attrs
-    @attachment = @admin.file_attachments.create(storage: @file)
-    @post.attachments << @attachment
-    @attachment.reload
+    @attachment_file = @admin.file_attachments.create(storage: @file)
+    @attachment_image = @admin.image_attachments.create(storage: @image)
+    @post.attachments << @attachment_file
+    @post.attachments << @attachment_image
+    @attachment_file.reload
+    @attachment_image.reload
   end
   it "should return 200 when attachment belongs to post" do
-    get 'show', id: @attachment.id, private_token: @user.private_token
+    get 'show', id: @attachment_file.id, private_token: @user.private_token
     response.status.should == 200
   end
   it "should return 200 when attachment belongs to user" do
-    get 'show', id: @attachment.id, private_token: @admin.private_token
+    get 'show', id: @attachment_file.id, private_token: @admin.private_token
     response.status.should == 200
   end
   it "should return 404" do
-    get 'show', id: @attachment.id, private_token: @user_another.private_token
+    get 'show', id: @attachment_file.id, private_token: @user_another.private_token
     response.status.should == 404
+  end
+  context "image" do
+    it "should return 200 when image_attachment exists" do
+      get 'show', id: @attachment_image.id, private_token: @admin.private_token
+      response.status.should == 200
+    end
+    it "should return 200 when verison exists" do
+      get 'show', id: @attachment_image.id, private_token: @admin.private_token, version: :mobile
+      response.status.should == 200
+    end
+    it "should return 404 when attachment not exist" do
+      get 'show', id: 'not_exist', private_token: @admin.private_token
+      response.status.should == 404
+    end
+    it "should return 404 when verison not exist" do
+      get 'show', id: @attachment_image.id, private_token: @admin.private_token, version: :not_exist
+      response.status.should == 404
+    end
   end
 end
