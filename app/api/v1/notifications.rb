fc7007b9a13a563c8ef0d6e968bad5e3440941d2
fahchen <dev.fah@gmail.com>
2013-07-26 16:17:13 +0800

    Fix #17: Add API to mark multiple notification as read

--- app/api/v1/notifications.rb
+++ app/api/v1/notifications.rb
@@ -2,6 +2,12 @@ class Notifications < Grape::API
   before { authenticate! }
 
   resource :notifications do
+    put :read do
+      notifications = current_user.notifications.where(:id.in => params[:notification_ids])
+      notifications.map { |notification| notification.read! }
+      status(204)
+    end
+
     route_param :id do
       before do
         @notification = current_user.notifications.where(id: params[:id]).first
--- spec/api/v1/notifications_spec.rb
+++ spec/api/v1/notifications_spec.rb
@@ -6,6 +6,7 @@ describe Youxin::API, 'notifications' do
   before(:each) do
     @user = create :user
     @notification = create :notification_base, user_id: @user.id
+    @notification_one = create :notification_base, user_id: @user.id
   end
   describe "PUT /notifications/:id/read" do
     it "should mark the notification as read" do
@@ -21,6 +22,20 @@ describe Youxin::API, 'notifications' do
     it "should return 404" do
       put api('/notifications/not_exists/read', @user)
       response.status.should == 404
+    end
+    it "should return 404" do
+      user_another = create :user
+      put api('/notifications/not_exists/read', user_another)
+      response.status.should == 404
+    end
+  end
+
+  describe "PUT /notifications/read" do
+    it "should mark the notifications as read" do
+      put api('/notifications/read', @user), notification_ids: [@notification, @notification_one].map(&:id)
+      response.status.should == 204
+      @notification.reload.read.should == true
+      @notification_one.reload.read.should == true
     end
   end
 end
