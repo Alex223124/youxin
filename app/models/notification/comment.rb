23b4e337d0ddf0d7624d2009ee21dad2c50d5658
fahchen <dev.fah@gmail.com>
2013-10-02 18:14:41 +0800

    add notification faye_payload

--- app/models/notification/comment.rb
+++ app/models/notification/comment.rb
@@ -5,20 +5,42 @@ class Notification::Comment < Notification::Base
 
   validates :comment_id, presence: true
 
-  after_create :send_comment_notification_to_ios_device
+  after_create :send_comment_notifications
 
   private
-  def send_comment_notification_to_ios_device
-    Notification::Notifier.publish_to_ios_device_async([self.user.id], ios_payload)
+  def send_comment_notifications
+    Notification::Notifier.publish_to_ios_device_async([user.id], ios_payload)
+    Notification::Notifier.publish_to_faye_client_async([user_id], faye_payload)
   end
   def ios_payload
     {
       alert: "#{comment.user.name} 评论了你的优信\n#{comment.body[0...20]}...",
       custom: {
         type: :comment,
-        id: self.id.to_s,
+        id: self.id.to_s
       }
     }
   end
-
+  def faye_payload
+    {
+      'comment_notification' =>
+        self.as_json(only: [:read, :created_at],
+                      methods: [:id],
+                      include: {
+                        comment: {
+                          only: [:commentable_type],
+                          include: {
+                            commentable: {
+                              only: [:title, :body, :created_at],
+                              methods: [:id],
+                            },
+                            user: {
+                              only: [:name],
+                              methods: [:id, :avatar_url]
+                            }
+                          }
+                        }
+                    })
+    }
+  end
 end
--- app/models/notification/organization.rb
+++ app/models/notification/organization.rb
@@ -11,11 +11,12 @@ class Notification::Organization < Notification::Base
   scope :_in, ->{ where(status: 'in') }
   scope :_out, ->{ where(status: 'out') }
 
-  after_create :send_organization_notification_to_ios_device
+  after_create :send_organization_notifications
 
   private
-  def send_organization_notification_to_ios_device
+  def send_organization_notifications
     Notification::Notifier.publish_to_ios_device_async([user.id], ios_payload)
+    Notification::Notifier.publish_to_faye_client_async([user.id], faye_payload)
   end
   def ios_payload
     {
@@ -24,6 +25,19 @@ class Notification::Organization < Notification::Base
         type: :organization,
         id: id.to_s,
       }
+    }
+  end
+  def faye_payload
+    {
+      'organization_notification' =>
+        self.as_json(only: [:read, :created_at, :status],
+                      methods: [:id],
+                      include: {
+                        organization: {
+                          only: [:name],
+                          methods: [:id, :avatar_url]
+                        }
+                    })
     }
   end
 end
