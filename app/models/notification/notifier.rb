35dcaa1ee40f94ae3d8cf5e093b012a42350b740
fahchen <dev.fah@gmail.com>
2013-10-14 17:10:41 +0800

    fix sms typo

--- app/models/notification/notifier.rb
+++ app/models/notification/notifier.rb
@@ -75,7 +75,7 @@ class Notification::Notifier
       receipt = Receipt.find(receipt) unless receipt.is_a? Receipt
       if receipt && receipt.user.phone?
         post = receipt.post
-        content = "#{post.author.name}: 您有一条优信[#{post.title}]。详情点击： #{receipt.short_url} 【Combee】"
+        content = "#{post.author.name}: 您有一条新优信[#{post.title}]。详情点击： #{receipt.short_url} 【Combee】"
         res = message(receipt.user.phone, content)
         CommunicationRecord::Sms.create receipt: receipt, status: res[:code]
       end
@@ -85,7 +85,7 @@ class Notification::Notifier
       receipt = Receipt.find(receipt) unless receipt.is_a? Receipt
       if receipt && receipt.user.phone?
         post = receipt.post
-        content = "您好！您收到了#{post.author.name}通过combee给您的电话留言：您有一条优信，#{post.title}，请尽快登陆combee网站或移动客户端查看详细信息。留言已结束，感谢您的收听，再见。"
+        content = "您好！您收到了#{post.author.name}通过combee给您的电话留言：您有一条新优信，#{post.title}，请尽快登陆combee网站或移动客户端查看详细信息。留言已结束，感谢您的收听，再见。"
 
         landing_call = call receipt.user.phone, media_txt: content
         call_sid = landing_call.response.body[:landing_call][:call_sid] rescue nil
