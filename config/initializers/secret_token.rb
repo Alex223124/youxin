475411e1fca9327bfa8710e4870c58311d932091
fahchen <dev.fah@gmail.com>
2013-12-23 21:00:43 +0800

    reset session secret token

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Youxin::Application.config.secret_token = 'aa51d9a0b2ab05c21e537ee359dbfc2b346d6f1f5eeef74db7795e48490f0dc142bf7b6bbda8c2e7413b8f6cdf9f00b3119a820edd4108d1dc943766c7062f5c'
+Youxin::Application.config.secret_token = '77d9a5d856fcf03bf89d013c21edf85f106cba3aaacf2af916af863ce1c6c23aef6b1e81753510fd5b0100ed88306a1ce677be296f2780180bb7d502ad109c7f'
